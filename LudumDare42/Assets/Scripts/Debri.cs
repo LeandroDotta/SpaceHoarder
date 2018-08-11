00fc84ac3b1a5b20dc9e1ca61041d528cd65f70e
Dalton Lima <daltonvarussa@gmail.com>
2018-08-11 20:21:28 -0300

    [wip] add Player movement

---
+++ LudumDare42/Assets/Scripts/Debri.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public enum Size
+{
+    Small,
+    Medium,
+    Large
+};
+
+public class Debri : MonoBehaviour, IGrabable
+{
+    public Size Size;
+    public float PlayerSpeedMultiplier = 1;
+    
+}
