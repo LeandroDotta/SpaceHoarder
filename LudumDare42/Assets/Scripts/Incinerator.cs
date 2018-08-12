0456befa2b56b154d029b92a701cb0744b827139
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2018-08-12 02:14:40 -0300

    Incinerator
    Implement Incinerator

---
+++ LudumDare42/Assets/Scripts/Incinerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Incinerator : MonoBehaviour {
+
+	public float CooldownCounter { get; private set; }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(CooldownCounter > 0)
+		{
+			CooldownCounter -= Time.deltaTime;
+
+			if(CooldownCounter < 0)
+				CooldownCounter = 0;
+		}
+	}
+
+	private void OnTriggerStay(Collider other) {
+		if (CooldownCounter > 0)
+			return;
+
+		if(other.CompareTag("Debri")) {
+			CooldownCounter = other.GetComponentInParent<Debri>().IncineratorCooldown;
+			Destroy(other.transform.parent.gameObject);
+		}
+	}
+}
