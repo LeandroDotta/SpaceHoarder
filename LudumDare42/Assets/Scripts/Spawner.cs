3dccad1ba66bad3c4d8758864788de7c13f32000
Dalton Lima <daltonvarussa@gmail.com>
2018-08-12 01:32:16 -0300

    improved spawner

--- LudumDare42/Assets/Scripts/Spawner.cs
+++ LudumDare42/Assets/Scripts/Spawner.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject[] VariousDebris;
+    public float[] DebrisSpawnRate;
     public GameObject SpawnPoint;
     public float SpawnMostWait;
     public float SpawnLeastWait;
     public int StartWait;
     public bool Stop;
-    public float BurstForce = 200f;
-
-    int _randomDebris;   
+    public float BurstForce = 200f;    
 
 	void Start ()
 	{
@@ -28,11 +27,24 @@ public class Spawner : MonoBehaviour
             Rigidbody rb;
             GameObject go;
 
-            _randomDebris = Random.Range(0, 3);
+            float random = Random.Range(0f, 1f);
+            int _randomDebrisIndex;
 
+            if (random < DebrisSpawnRate[0])
+            {
+                _randomDebrisIndex = 0;
+            } else if (random < DebrisSpawnRate[0] + DebrisSpawnRate[1])
+            {
+                _randomDebrisIndex = 1;
+            }
+            else
+            {
+                _randomDebrisIndex = 2;
+            }
             float randomInterval = Random.Range(SpawnLeastWait, SpawnMostWait);
-            go = (GameObject)Instantiate(VariousDebris[_randomDebris], SpawnPoint.transform.position, gameObject.transform.rotation);
-            go.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(SpawnPoint.transform.forward) * BurstForce * -1);
+            go = (GameObject)Instantiate(VariousDebris[_randomDebrisIndex], SpawnPoint.transform.position, gameObject.transform.rotation);
+            rb = go.GetComponent<Rigidbody>();
+            rb.AddForce(transform.TransformDirection(SpawnPoint.transform.forward) * BurstForce * -1 * rb.mass);
             Debug.Log(transform.TransformDirection(SpawnPoint.transform.forward));
 
             yield return new WaitForSeconds(randomInterval);   
