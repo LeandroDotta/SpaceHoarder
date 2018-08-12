f5c4842238716efe274506660e76c1faec9082e9
Dalton Lima <daltonvarussa@gmail.com>
2018-08-12 14:26:57 -0300

    Updated Debri Score GameManager now DontDestroyOnLoad

--- LudumDare42/Assets/Scripts/Debri.cs
+++ LudumDare42/Assets/Scripts/Debri.cs
@@ -12,7 +12,7 @@ public class Debri : MonoBehaviour, IGrabable
     public Size Size;
     public float PlayerSpeedMultiplier = 1;
     public float IncineratorCooldown = 1.0f;
-    private int _score = 1;
+    [SerializeField] private int _score = 1;
 
     public int Score
     {
