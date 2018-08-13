9764157da10bae83073af19be8fa499d51554350
Dalton Lima <daltonvarussa@gmail.com>
2018-08-13 10:02:36 -0300

    Polishing IntroScene

--- LudumDare42/Assets/_SCRIPTS/Menu.cs
+++ LudumDare42/Assets/_SCRIPTS/Menu.cs
@@ -1,20 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Menu : MonoBehaviour {
+public class Menu : MonoBehaviour
+{
 	[SerializeField] private string _sceneName;
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		if (CrossPlatformInputManager.GetButton("Submit")) {
-			SceneManager.LoadScene(_sceneName);
+	void Update ()
+    {
+		if (CrossPlatformInputManager.GetButton("Submit"))
+        {
+            if (_sceneName != null)
+            {
+			    SceneManager.LoadScene(_sceneName);
+                return;
+            }
+            else
+            {
+                SceneManager.LoadScene(1);
+            }
 		}
 	}
 }
