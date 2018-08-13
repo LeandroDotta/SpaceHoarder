ad260523ec947edb8de7878471908a3c24ae0c5e
wflorencio <chimerarj@gmail.com>
2018-08-13 02:54:22 -0300

    Generalized organization in all folders, new title screen, main scene with new assets, new assests for debrys, exclusive scene for tests with debrys physics, statemachine enum in gamemanager script.

---
+++ LudumDare42/Assets/_SCRIPTS/GUIBar.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum BarType
+{
+	MessBar,
+	Incinerator,
+	Compactor
+};
+
+
+public class GUIBar : MonoBehaviour {
+
+	Image bar;
+	BarType bartype;
+
+
+    private int _maxRawValue = 1000;
+
+    public int MaxRawValue
+    {
+        get { return _maxRawValue; }
+        set { _maxRawValue = value; }
+    }
+
+
+    void Start()
+	{
+		bar = this.gameObject.GetComponent<Image>();
+
+        setBarToZero();
+
+        if (bartype == BarType.Incinerator)
+		{
+			
+		}
+	}
+		
+
+	public void UpdateMess(float value)
+	{
+        //Assim que haver variáveis de bagunça, descomentar esse bloco
+        //Debug.Log("value: " + value);
+        //Debug.Log("%: " + value / _maxRawValue);
+
+        if (value > 0)
+        {
+            bar.fillAmount = (float)value / (float)_maxRawValue; //tlvz messlevel tenha que ser subtraído ou dividido ao invés de multiplicado..
+            ChangeBarColor();
+        }
+        else
+        {
+            bar.fillAmount = 0;
+        }
+
+        
+
+	}
+
+    public void setBarToZero()
+    {
+        bar.fillAmount = 0;
+    }
+
+
+	public void UpdateCooldownBar(float cooldown)
+	{
+		bar.fillAmount = ((cooldown * 1.0f) / (100 * 1.0f)); //tlvz messlevel tenha que ser subtraído ou dividido ao invés de multiplicado..
+	}
+
+	public void ChangeBarColor()
+	{
+		if (bar.fillAmount < 0.30f)
+		{
+            bar.color = new Color32(0, 245, 63, 255);
+        }
+		else if (bar.fillAmount < 0.55f) 
+		{
+			bar.color = new Color32 (245, 207, 0, 255);
+		}
+		else if(bar.fillAmount > 0.56f) 
+		{
+            bar.color = new Color32(255, 50, 0, 255);
+		}		
+	}
+}
---
+++ LudumDare42/Assets/_SCRIPTS/GameManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Text _scoreText;
+    [SerializeField] private Text _WaveText;
+
+	public enum StateMachine { Title, Game, Pausa, GameOver };
+	public StateMachine state = StateMachine.Title;
+
+    public GUIBar MessBar;  // Mess bar at TopRightPanel
+    public Spawner[] waveSpawn;
+    private int _score = 0;
+    private int debriCount = 0;
+    private int actualMessValue = 0;
+    private int accruedMessValue = 0;
+
+    private int _waveNumber = 1;
+    private int _maxMessValue = 25;
+    private float _totalWaveMessValue = 25;
+
+    public int MaxMessValue {
+        get { return _maxMessValue; }
+        set { _maxMessValue = value; }
+    }
+
+    public float totalWaveMessValue
+    {
+        get { return _totalWaveMessValue; }
+        set { _totalWaveMessValue = value; }
+    }
+
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        Init();
+    }
+
+
+    public void Init()
+    {
+        MessBar.MaxRawValue = _maxMessValue;
+        _WaveText.text = _waveNumber.ToString();
+        //MessBar.UpdateMess();
+    }
+
+	public void AddScore(int value)
+	{
+		_score += value;
+		_scoreText.text = _score.ToString();
+	}
+
+    public void UpdateMessStatus(int value)
+    {
+
+        //Debug.Log("INCREMENTO: " + value + " - TOTAL: " + (actualMessValue + value));
+        if (value > 0)
+        {
+            accruedMessValue += value;
+        }
+
+        actualMessValue += value;
+        MessBar.UpdateMess((float)actualMessValue);
+
+        if (IsGameOver())
+        {
+            Debug.Log("GAME OVE!!!!!!");
+        }
+
+        if (EndWave())
+        {
+            StartNewWave();
+        }
+
+    }
+
+    bool IsGameOver()
+    {
+        return actualMessValue > _maxMessValue;
+    }
+
+    bool EndWave()
+    {
+        return accruedMessValue > _totalWaveMessValue;
+    }
+
+    public void StartNewWave()
+    {
+
+        foreach (var Spawner in waveSpawn)
+        {
+            Spawner.SpawnLeastWait *= 0.90f;
+            Spawner.SpawnMostWait *= 0.90f;
+            Spawner.DebrisSpawnRate[0] -= 0.05f;
+            Spawner.DebrisSpawnRate[1] += 0.03f;
+            Spawner.DebrisSpawnRate[2] += 0.02f;
+        }
+
+        _totalWaveMessValue = _totalWaveMessValue * 1.5f;
+
+        accruedMessValue = 0;
+        actualMessValue = 0;
+        _waveNumber += 1;
+
+        MessBar.setBarToZero();
+
+        _WaveText.text = _waveNumber.ToString();
+
+        Debug.Log("WAVE: " + _waveNumber);
+    }
+
+}
